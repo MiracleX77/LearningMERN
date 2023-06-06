345ce1c1344b60f361b7e15b40f961e01584af3b
Phatchara_Mix <99187452+MiracleX77@users.noreply.github.com>
2023-06-06 18:24:34 +0700

    .

--- MERN_Basic/server/controllers/blogController.js
+++ MERN_Basic/server/controllers/blogController.js
@@ -1,8 +1,11 @@
 const slugify = require("slugify")
+const Blogs = require("../models/blogs")
+const {v4:uuidv4} = require("uuid")
 //บันถึงข้อมูล
-exports.create = (req,res) =>{
+exports.create =  async (req,res) =>{
     const {title,content,author} = req.body
-    const slug = slugify(title)
+    let slug = slugify(title) || uuidv4()
+
     //validate
     switch(true){
         case !title:
@@ -10,10 +13,58 @@ exports.create = (req,res) =>{
             break;
         case !content:
             return res.status(400).json({error : "กรุณาป้อนเนื้อหาบทความ"})
-            break;
-
+            break;        
     }
-    res.json({
-        data:{title,content,author,slug}
-    })
+
+    try{
+        const dataBlog = await Blogs.create({title,content,author,slug})
+        res.json(dataBlog)
+    }
+    catch (err){
+        res.status(400).json({error:"ชื่อบทความซ้ำกัน"})
+    }
+}
+
+exports.getAllblogs= async (req,res) =>{
+    try{
+        const dataBlogs = await Blogs.find({}).exec()
+        res.json(dataBlogs)
+    }
+    catch(err){
+        res.json(err)
+    }
+}
+
+exports.getSingleBlog = async (req,res)=>{
+    try{
+        const {slug} = req.params
+        const datablog =  await Blogs.findOne({slug}).exec()
+        res.json(datablog)
+    }
+    catch (err){
+        res.json(err)
+    }
+}
+
+exports.removeBlog = async(req,res) =>{
+    try{
+        const {slug} = req.params
+        await Blogs.findOneAndRemove({slug}).exec()
+        res.json({message:"remove complete"})
+    }
+    catch (err){
+        res.json(err)
+    }
+}
+
+exports.updateBlog= async (req,res)=>{
+    const {slug} = req.params
+    const {title,content,author} = req.body
+    try{
+        const dataBlog = await Blogs.findOneAndUpdate({slug},{title,content,author},{new:true}).exec()
+        res.json(dataBlog)
+    }
+    catch (err){
+        res.json(err)
+    }
 }
--- MERN_Basic/server/routes/blog.js
+++ MERN_Basic/server/routes/blog.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {create} =require('../controllers/blogController')
+const {create,getAllblogs,getSingleBlog,removeBlog,updateBlog} =require('../controllers/blogController')
 
 router.post('/create',create)
+router.get("/blogs",getAllblogs)
+router.get("/blog/:slug",getSingleBlog)
+router.delete('/blog/:slug',removeBlog)
+router.put('/blog/:slug',updateBlog)
 
 module.exports = router
